740396ba482107c154e6bad0797e4ab5b5e4702f
airatlovesmusic <airatlovesmusic@gmail.com>
2020-08-21 19:03:21 +0300

    Add articles json for backend imitation

--- data/src/main/java/com/airatlovesmusic/data/network/ApiService.kt
+++ data/src/main/java/com/airatlovesmusic/data/network/ApiService.kt
@@ -3,6 +3,7 @@ package com.airatlovesmusic.data.network
 import com.airatlovesmusic.model.Article
 
 interface ApiService {
+
     fun getArticles(): List<Article>
     fun getArticle(url: String): Article?
 }
